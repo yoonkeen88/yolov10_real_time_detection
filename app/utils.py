66018c16f181d794023a538bf6c96542f04b6950
yoonkeen <agy91521947@gmail.com>
2024-08-16 22:03:01 +0900

    real time detecting

--- app/utils.py
+++ app/utils.py
@@ -8,18 +8,25 @@ model = YOLO('/Users/angwang-yun/Desktop/Project/yolov10_real_time_detection/yol
 
 def gen_frames():
     cap = cv2.VideoCapture(0)  # Use 0 for webcam
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return  # 웹캠을 열 수 없는 경우 함수 종료
+    
     while True:
         success, frame = cap.read()
         if not success:
             break
         results = model(frame)
-        annotated_frame = results.render()[0]
+        # YOLOv8 이후 버전에서는 이미지에 직접 그리기
+        for result in results:
+            annotated_frame = result.plot()  # result.render() 대신 result.plot() 사용
         # Resize frame to 1000x1000
         annotated_frame = cv2.resize(annotated_frame, (1000, 1000))
         ret, buffer = cv2.imencode('.jpg', annotated_frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 def process_video(filepath):
     # Create output file path
