06499ad8e056c22759e24de876c0ffd662dd4914
light <agy91521947@gmail.com>
2024-08-18 21:01:49 +0900

    error page

--- app/utils.py
+++ app/utils.py
@@ -1,32 +1,51 @@
+import os
+import time
 import cv2
 import numpy as np
 from ultralytics import YOLO
-import os
+# from flask import Flask, render_template, Response, jsonify
 
-# Load your YOLO model here
+# YOLO 모델 로드
 model = YOLO('best.pt')
 
+# app = Flask(__name__)
+
+# "drowsy" 감지 시간 저장을 위한 리스트
+drowsy_detections = []
+
 def gen_frames():
-    cap = cv2.VideoCapture(0)  # Use 0 for webcam
+    cap = cv2.VideoCapture(0)  # 웹캠 사용 (0번 카메라)
     if not cap.isOpened():
         print("Error: Could not open webcam.")
-        return  
-    
+        return
+
     while True:
         success, frame = cap.read()
         if not success:
             break
-        results = model(frame)
         
+        results = model(frame)
+
+        # 현재 시간
+        current_time = time.time()
+        global drowsy_detections
+
         for result in results:
-            annotated_frame = result.plot() 
-        # Resize frame to 1000x1000
+            for box in result.boxes:
+                if 'drowsy' in result.names[int(box.cls)]:
+                    drowsy_detections.append(current_time)
+                    # 10초가 지난 감지 시간은 리스트에서 제거
+                    drowsy_detections = [t for t in drowsy_detections if current_time - t <= 10]
+                    break
+            
+            annotated_frame = result.plot()
+
+        # 프레임 크기 조정 (1000x1000)
         annotated_frame = cv2.resize(annotated_frame, (1000, 1000))
         ret, buffer = cv2.imencode('.jpg', annotated_frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 
 def process_video(filepath):
     # Create output file path
